7c26431c23fa61c685d6d3cde373c95bf9700498
beer <bereri@norlys.dk>
2021-12-02 21:47:44 +0100

    compass arrow

--- app/src/main/java/dk/au/mad21fall/appproject/group3/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/dk/au/mad21fall/appproject/group3/ui/notifications/NotificationsFragment.java
@@ -2,8 +2,12 @@ package dk.au.mad21fall.appproject.group3.ui.notifications;
 
 import static android.content.Context.SENSOR_SERVICE;
 
+import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,13 +30,19 @@ public class NotificationsFragment extends Fragment {
     private FragmentNotificationsBinding binding;
     private ImageView compasspointer;
     private SensorManager sensorManager;
+    private LocationManager locationManager;
     private Sensor sensoraccelerometer;
     private Sensor sensormagneticfield;
+    private Context mContext;
 
     private float[] Gravity = new float[3];
     private float[] GeoMagnetic = new float[3];
     private float[] Orientation = new float[3];
     private float[] Rotation = new float[3];
+
+    public NotificationsFragment(Context mContext) {
+        this.mContext = mContext;
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -40,9 +50,44 @@ public class NotificationsFragment extends Fragment {
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
 
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
-        //compasspointer.findViewById(R.id.compasspointer);
-        //sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        compasspointer.findViewById(R.id.compasspointer);
+        sensorManager = (SensorManager) mContext.getSystemService(SENSOR_SERVICE);
+        locationManager = (LocationManager)mContext.getSystemService(mContext.LOCATION_SERVICE);
+
+        sensoraccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensormagneticfield = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
         View root = binding.getRoot();
+
+        SensorEventListener sensorEventListeneraccelerometer = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                Gravity = sensorEvent.values;
+                SensorManager.getRotationMatrix(Rotation,null,Gravity,GeoMagnetic);
+                SensorManager.getOrientation(Rotation,Orientation);
+                compasspointer.setRotation((float) (-Orientation[0]*180/3.14159));
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            }
+        };
+
+        SensorEventListener sensorEventListenermagneticfield = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+            GeoMagnetic = sensorEvent.values;
+            SensorManager.getRotationMatrix(Rotation,null,Gravity,GeoMagnetic);
+            SensorManager.getOrientation(Rotation,Orientation);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+        sensorManager.registerListener(sensorEventListeneraccelerometer,sensoraccelerometer,SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(sensorEventListenermagneticfield,sensormagneticfield,SensorManager.SENSOR_DELAY_NORMAL);
 
         //final TextView textView = binding.textNotifications;
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -60,4 +105,7 @@ public class NotificationsFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+
+
 }
