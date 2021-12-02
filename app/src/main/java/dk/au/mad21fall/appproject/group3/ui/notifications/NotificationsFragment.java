977412ec1042cc710328e533da5f30c317ed2fb5
beer <bereri@norlys.dk>
2021-12-02 16:10:18 +0100

    compass shit

--- app/src/main/java/dk/au/mad21fall/appproject/group3/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/dk/au/mad21fall/appproject/group3/ui/notifications/NotificationsFragment.java
@@ -1,9 +1,14 @@
 package dk.au.mad21fall.appproject.group3.ui.notifications;
 
+import static android.content.Context.SENSOR_SERVICE;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,6 +24,15 @@ public class NotificationsFragment extends Fragment {
 
     private NotificationsViewModel notificationsViewModel;
     private FragmentNotificationsBinding binding;
+    private ImageView compasspointer;
+    private SensorManager sensorManager;
+    private Sensor sensoraccelerometer;
+    private Sensor sensormagneticfield;
+
+    private float[] Gravity = new float[3];
+    private float[] GeoMagnetic = new float[3];
+    private float[] Orientation = new float[3];
+    private float[] Rotation = new float[3];
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,13 +40,16 @@ public class NotificationsFragment extends Fragment {
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
 
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        compasspointer.findViewById(R.id.compasspointer);
+        //sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
+        //final TextView textView = binding.textNotifications;
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onChanged(@Nullable String direction)
+            {
+                //textView.setText(direction);
             }
         });
         return root;
