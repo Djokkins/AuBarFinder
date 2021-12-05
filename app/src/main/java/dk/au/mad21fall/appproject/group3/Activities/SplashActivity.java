e1181399a698c5750552c61595cacb2ec3460886
Biorrith <201909298@post.au.dk>
2021-12-05 10:34:49 +0100

    Animations uwu

--- app/src/main/java/dk/au/mad21fall/appproject/group3/Activities/SplashActivity.java
+++ app/src/main/java/dk/au/mad21fall/appproject/group3/Activities/SplashActivity.java
@@ -4,12 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import dk.au.mad21fall.appproject.group3.Models.Repository;
 import dk.au.mad21fall.appproject.group3.R;
 
 public class SplashActivity extends AppCompatActivity {
+
+    Animation leftAnimation, rightAnimation, leftDropAnim, rightDropAnim;
+    ImageView imgBeerLeft, imgBeerRight, imgDropsLeft, imgDropsRight;
+
 
     //https://abhiandroid.com/programming/splashscreen
     @Override
@@ -21,25 +32,71 @@ public class SplashActivity extends AppCompatActivity {
         Repository repository;  //repository
         repository = Repository.getInstance();  //get Repository singleton
 
-        Thread welcomeThread = new Thread() {
+        imgBeerLeft = findViewById(R.id.imgBeerLeft);
+        imgBeerRight = findViewById(R.id.imgBeerRight);
+        imgDropsLeft = findViewById(R.id.imgDropsLeft);
+        imgDropsRight = findViewById(R.id.imgDropsRight);
+        imgDropsLeft.setVisibility(View.INVISIBLE);
+        imgDropsRight.setVisibility(View.INVISIBLE);
+        leftAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_left);
+        rightAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_right);
+        leftDropAnim = AnimationUtils.loadAnimation(this, R.anim.appear_left);
+        rightDropAnim = AnimationUtils.loadAnimation(this, R.anim.appear_right);
+
+
+        imgBeerLeft.setAnimation(leftAnimation);
+        imgBeerRight.setAnimation(rightAnimation);
+
+        leftAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imgDropsLeft.setAnimation(leftDropAnim);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        rightAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imgDropsRight.setAnimation(rightDropAnim);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+
+
+        //https://www.youtube.com/watch?v=JLIFqqnSNmg&ab_channel=CodingWithTea
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                try {
-                    super.run();
-                    sleep(2000);  //Delay of 10 seconds
-                } catch (Exception e) {
+                Intent i;
+                if(FirebaseAuth.getInstance().getCurrentUser() != null) {
+                    i = new Intent(SplashActivity.this,MainActivity.class);
+                } else i = new Intent(SplashActivity.this,LoginActivity.class);
 
-                } finally {
-                    Intent i = new Intent(SplashActivity.this,
-                            LoginActivity.class);
-                    startActivity(i);
-                    finish();
-                }
+                startActivity(i);
+                //overridePendingTransition(R.anim.activity_change_zoom, 300);
+                finish();
             }
-        };
-        welcomeThread.start();
-
-
+        }, 3000); //Time for it to run
 
 
     }
